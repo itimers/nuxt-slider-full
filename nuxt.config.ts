d957ca713e5dcd60d9f1f225665fcd47d1329205
George Stojanovic <94017086+itimers@users.noreply.github.com>
2025-12-02 17:54:11 +0100

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,23 @@
+//@ts-ignore
+const isProd = process.env.NODE_ENV === 'production'
+const base = isProd ? '/nuxt-cookie-system/' : '/'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@pinia/nuxt'],
+  app: {
+    baseURL: base,
+    head: {
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: `${base}favicon.ico`,
+        },
+      ],
+    }
+  },
+  nitro: {
+    preset: 'github-pages',
+  }
 })
